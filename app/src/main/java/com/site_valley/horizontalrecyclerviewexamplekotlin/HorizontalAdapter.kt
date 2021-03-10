f1bf6bd6c055b622fcc99aec4ba9b2df6f42d81a
Abhinav <xabhi7549@gmail.com>
2021-03-10 12:29:32 +0530

    Initial commit

---
+++ app/src/main/java/com/site_valley/horizontalrecyclerviewexamplekotlin/HorizontalAdapter.kt
@@ -0,0 +1,44 @@
+package com.site_valley.horizontalrecyclerviewexamplekotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+
+class HorizontalAdapter(val list: ArrayList<String>) :
+    RecyclerView.Adapter<HorizontalAdapter.MyView>() {
+    class MyView(view: View) : RecyclerView.ViewHolder(view) {
+        var imageView: ImageView
+        init {
+            imageView = view
+                .findViewById<ImageView>(R.id.image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
+        val itemView: View = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.recycler_item,
+                parent,
+                false
+            )
+        return MyView(itemView)
+    }
+
+        override fun onBindViewHolder(holder: MyView, position: Int) {
+            val listData = list[position]
+
+            //Loading Image into view
+            Picasso.get().load(listData).placeholder(R.mipmap.ic_launcher).into(holder.imageView)
+
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+}
---
+++ app/src/main/java/com/site_valley/horizontalrecyclerviewexamplekotlin/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.site_valley.horizontalrecyclerviewexamplekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //creating recycler val
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+
+        //setting recycler to horizontal scroll
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        //creating a  arraylist of data
+        val data: ArrayList<String> = ArrayList()
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+
+        //setting adapter to recycler
+        recycler.adapter = HorizontalAdapter(data)
+
+    }
+}
